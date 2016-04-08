e04c1e74c254d8a755930d53e07296366c0badee
elmo911 <jacobmwolfe@gmail.com>
2016-04-08 14:33:15 -0500

    more

---
+++ app/src/main/java/aitp/geohunt/Callbacks/AlertCallBack.java
@@ -0,0 +1,8 @@
+package aitp.geohunt.Callbacks;
+
+public interface AlertCallBack {
+
+    public void alertHelperTextResponse(String title, String response);
+
+    public void alertHelperBooleanResponse(String title, Boolean response);
+}
