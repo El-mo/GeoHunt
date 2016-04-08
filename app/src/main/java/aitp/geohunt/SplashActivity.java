7696329871bd34862e0207662c4efecdf274c23d
elmo911 <jacobmwolfe@gmail.com>
2016-04-08 14:44:07 -0500

    splash

--- app/src/main/java/aitp/geohunt/SplashActivity.java
+++ app/src/main/java/aitp/geohunt/SplashActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private final int SPLASH_TIME = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
