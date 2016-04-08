5a863596adcbd7bfa3bd9371b412f11784038d50
elmo911 <jacobmwolfe@gmail.com>
2016-04-08 14:13:58 -0500

    Initial commit

---
+++ app/src/main/java/aitp/geohunt/SplashActivity.java
@@ -0,0 +1,12 @@
+package aitp.geohunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
